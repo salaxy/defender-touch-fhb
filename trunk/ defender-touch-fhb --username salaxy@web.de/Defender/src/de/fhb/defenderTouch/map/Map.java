de38bfc42d1330c6bbe4435d31e18720683afc2d
mertens@fh-brandenburg.de <mertens@fh-brandenburg.de@b545fbdc-7392-c354-f912-c6a15991a2d7>
2011-10-21 15:20:06 +0000

    MapComponent aufgebohrt. ToDo: vom Spieler abhängigen Sichtbereich zeichnen (Spielerobjekte fehlen noch)

--- trunk/ defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/map/Map.java
+++ trunk/ defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/map/Map.java
@@ -1,5 +1,6 @@
 package de.fhb.defenderTouch.map;
 
+import java.util.ArrayList;
 import processing.core.PApplet;
 import processing.core.PVector;
 import de.fhb.defenderTouch.interfaces.Drawable;
@@ -8,62 +9,61 @@ public class Map implements  Drawable{
 	/**
 	 *  Das Applet, auf dem diese Karte anzeigt werden soll.
 	 */
-	protected PApplet display;
+	protected static PApplet display;
 	
 	/**
 	 * Blickrichtung
 	 */
-	protected float angle = 0;
+	protected static float angle = 0;
 	
 	/**
 	 * Position der linken oberen Kartenecke.
 	 */
-	protected PVector position = new PVector(16, 16);
+	protected static PVector position = new PVector(16, 16);
 
 	/**
 	 * Array mit allen Components (Kartenbestandteile)
 	 */
-	protected MapComponent[][] karte = new MapComponent[32][24];
+	protected static ArrayList<MapComponent> karte = new ArrayList<MapComponent>();
+	
+	/**
+	 * Konstante des linken Spielers, um seinen Teil des Bildschirms zu identifizieren.
+	 */
+	protected static boolean PLAYERONE = false;
+
+	/**
+	 * Konstante des rechten Spielers, um seinen Teil des Bildschirms zu identifizieren.
+	 */
+	protected static boolean PLAYERTWO = true;
 	
 	/**
 	 * Konstruktor
+	 * Wenn die Karte bereits existiert, wird sie nicht(!) neu erzeugt, stattdessen bekommt man ein weiteres Handle für die aktuelle Karte.
 	 * @param width Breite (32 für 1024x768)
 	 * @param height Höhe (24 für 1024x768)
 	 */
 	public Map(PApplet display, int width, int height) {
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				karte[x][y] = new MapComponent(display, this);
-			}
-		}
+		Map.display = display;
+		if (karte.isEmpty())
+			for (int x = 0; x < width; x++)
+				for (int y = 0; y < height; y++)
+					karte.add(new MapComponent(new PVector(x * 32 + 16, y * 32 + 16)));
 	}
 	
 	/**
 	 * Karte mit allen Components zeichnen.
 	 */
 	public void paint() {
-		for (int x = 0; x < karte.length; x++) {
-			for (int y = 0; y < karte[0].length; y++) {
-				karte[x][y].paint(new PVector(position.x + x * 32, position.y + y * 32));
-			}		
+		for (MapComponent element : karte) {
+			element.paint();
 		}
+		
+		paintView(PLAYERONE);
+		paintView(PLAYERTWO);
 	}
 	
-	/**
-	 * 
-	 */
-	public PVector WorldCoordinatesToScreenCoordinates(PVector worldCoodinates) {
-	
-		
-		return new PVector();
-	}
-	
-	/**
-	 * 
-	 */
-	public PVector ScreenCoodinatesToMapCoodinates(PVector screenCoordinates) {
-		
-		
-		return new PVector();
+	private void paintView(boolean whichOne) {
+		// passendes Spielerobjekt abfragen, um seinen Sichtbereich und Zoommodus zu erfahren
+		// dann zeichnen
 	}
 }
