14dfa8a13c794dafa1ffdc458ac4d98e64a35426
kwoxer <kwoxer@b545fbdc-7392-c354-f912-c6a15991a2d7>
2011-10-26 14:51:13 +0000

    Price statisch gesetzt

--- trunk/ defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/grounded/Ground.java
+++ trunk/ defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/grounded/Ground.java
@@ -2,30 +2,29 @@ package de.fhb.defenderTouch.units.grounded;
 
 import processing.core.PApplet;
 
+public class Ground extends Building {
 
-public class Ground extends Building{
+	public static final int PRICE = 50;
 
-	
-	public Ground(int x, int y, int mode, int playerID, PApplet disp){
+	public Ground(int x, int y, int mode, int playerID, PApplet disp) {
 		super(x, y, mode, playerID, disp);
-		this.price= 50;
 		// TODO Auto-generated constructor stub
 	}
 
-	public void drawFigure(){
-		
-//		display.stroke(0);
-//		display.strokeWeight(10);
-//		display.fill(0);
+	public void drawFigure() {
+
+		// display.stroke(0);
+		// display.strokeWeight(10);
+		// display.fill(0);
 		display.noFill();
-		display.rotate((float)Math.PI);
+		display.rotate((float) Math.PI);
 		this.entscheideLineFarbe();
 		display.scale(1);
-		display.triangle(-5,+5, 0, -5, +5, +5);
-		
+		display.triangle(-5, +5, 0, -5, +5, +5);
+
 		display.resetMatrix();
 		display.stroke(0);
-		
+
 	}
-	
+
 }
