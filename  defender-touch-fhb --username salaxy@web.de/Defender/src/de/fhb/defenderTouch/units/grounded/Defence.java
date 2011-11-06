29fa3739f1d352e251511f5fcf5501b27d92c889
kwoxer <kwoxer@b545fbdc-7392-c354-f912-c6a15991a2d7>
2011-11-06 15:50:10 +0000

    Gebäudegröße Level abhängig,und nice change in menu

---  defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/grounded/Defence.java
+++  defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/grounded/Defence.java
@@ -8,52 +8,48 @@ import processing.core.PVector;
 import de.fhb.defenderTouch.gamelogic.DefenderControl;
 import de.fhb.defenderTouch.graphics.GraphicTools;
 
-public class Defence extends Building{
+public class Defence extends Building {
 
 	public static final int PRICE = 40;
-	
-	public Defence(int x, int y, int mode, int playerID,DefenderControl gamelogic) {
+	protected int size = 0;
+
+	public Defence(int x, int y, int mode, int playerID, DefenderControl gamelogic) {
 		super(x, y, mode, playerID, gamelogic);
 	}
 
-	
-	
-	public void drawFigure( PGraphics graphics){
-		
-		switch(this.level){
-			case LEVEL_ONE:	//zeichne sooo;
-				;
+	public void drawFigure(PGraphics graphics) {
+
+		// this.entscheideLineFarbe(graphics);
+		graphics.scale(1.5f);
+		// graphics.strokeWeight(1);
+		graphics.rotate((float) Math.PI);
+		switch (this.level) {
+		case LEVEL_ONE:
+			size = 4;
 			break;
-			case LEVEL_TWO: //dann zeichne soo;
-				;
+		case LEVEL_TWO:
+			size = 6;
 			break;
-			case LEVEL_THREE://oder soooo;
-				;
+		case LEVEL_THREE:
+			size = 8;
 			break;
 		}
 
-//		this.entscheideLineFarbe(graphics);
-		graphics.scale(1.5f);
-		graphics.rotate((float)Math.PI);
-		ArrayList<PVector> vektoren=new ArrayList<PVector>();
-		vektoren.add(new PVector(-4, -4));
-		vektoren.add(new PVector(4, -4));
-		vektoren.add(new PVector(0,-4));
-		vektoren.add(new PVector(0,-8));
-		vektoren.add(new PVector(0, 4));
-		vektoren.add(new PVector(4,4));
-		vektoren.add(new PVector(-4,4));
-		
-		
+		ArrayList<PVector> vektoren = new ArrayList<PVector>();
+		vektoren.add(new PVector(-size, -size));
+		vektoren.add(new PVector(size, -size));
+		vektoren.add(new PVector(0, -size));
+		vektoren.add(new PVector(0, -size * 2));
+		vektoren.add(new PVector(0, size));
+		vektoren.add(new PVector(size, size));
+		vektoren.add(new PVector(-size, size));
 		graphics.noFill();
-		graphics.ellipse(0,-4 , 8, 8);
+		graphics.ellipse(0, -size, size * 2, size * 2);
+		GraphicTools.zeicheFigurNachVektoren(vektoren, graphics);
 
-		GraphicTools.zeicheFigurNachVektoren(vektoren,graphics);
-		
 		graphics.resetMatrix();
 		graphics.stroke(0);
-		
+
 	}
-	
 
 }
---  defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/grounded/Support.java
+++  defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/grounded/Support.java
@@ -11,66 +11,73 @@ import processing.core.PApplet;
 import processing.core.PGraphics;
 import processing.core.PVector;
 
-public class Support extends Building{
+public class Support extends Building {
 
 	public static final int PRICE = 30;
-	
-	private int frameTimesToRaiseCredits=50;
-	private int framesTemp=0;
-	
-	private int creditsPerTime=20;
-	
-	public Support(int x, int y, int mode, int playerID,DefenderControl gamelogic) {
+	protected int size = 0;
+
+	private int frameTimesToRaiseCredits = 50;
+	private int framesTemp = 0;
+
+	private int creditsPerTime = 20;
+
+	public Support(int x, int y, int mode, int playerID, DefenderControl gamelogic) {
 		super(x, y, mode, playerID, gamelogic);
 	}
 
-	
-	public void drawFigure(PGraphics graphics){
-		
-
-		
-		//farbewechsel bei Aktivierung
-//		this.entscheideLineFarbe( graphics);
-		//Skalieren
+	public void drawFigure(PGraphics graphics) {
+		// farbewechsel bei Aktivierung
+		// this.entscheideLineFarbe( graphics);
+		// Skalieren
 		graphics.scale(1.5f);
-		//Nach norden ausrichten
-		graphics.rotate((float)Math.PI);
-		
-		//Punkte hinzufuegen
-		ArrayList<PVector> vektoren=new ArrayList<PVector>();
-		vektoren.add(new PVector(0, -8));
-		vektoren.add(new PVector(0, 8));
-		vektoren.add(new PVector(0, 0));
-		vektoren.add(new PVector(8,0));
-		vektoren.add(new PVector(-8, 0));
+		// graphics.strokeWeight(1);
+		// Nach norden ausrichten
+		graphics.rotate((float) Math.PI);
 
-		//zeichnen
-		GraphicTools.zeicheFigurNachVektoren(vektoren,graphics);
-		
-		//zurücksetzen der Umgebungseinstellungen
+		switch (this.level) {
+		case LEVEL_ONE:
+			size = 8;
+			break;
+		case LEVEL_TWO:
+			size = 10;
+			break;
+		case LEVEL_THREE:
+			size = 12;
+			break;
+		}
+
+		// Punkte hinzufuegen
+		ArrayList<PVector> vektoren = new ArrayList<PVector>();
+		vektoren.add(new PVector(0, -size));
+		vektoren.add(new PVector(0, size));
+		vektoren.add(new PVector(0, 0));
+		vektoren.add(new PVector(size, 0));
+		vektoren.add(new PVector(-size, 0));
+
+		// zeichnen
+		GraphicTools.zeicheFigurNachVektoren(vektoren, graphics);
+
+		// zurücksetzen der Umgebungseinstellungen
 		graphics.resetMatrix();
 		graphics.stroke(0);
-		
 	}
-	
-	
-	 public void calcNewPosition(){
 
-		 	framesTemp++;				
-		 	
-		 	if(framesTemp==frameTimesToRaiseCredits){
-				if(this.playerID==DefenderControl.PLAYER_ONE){
-					gamelogic.getPlayerOne().addCredits(this.creditsPerTime);
-				}
-	
-				if(this.playerID==DefenderControl.PLAYER_TWO){
-					gamelogic.getPlayerTwo().addCredits(this.creditsPerTime);
-				}
-				
-				framesTemp=0;
+	public void calcNewPosition() {
+
+		framesTemp++;
+
+		if (framesTemp == frameTimesToRaiseCredits) {
+			if (this.playerID == DefenderControl.PLAYER_ONE) {
+				gamelogic.getPlayerOne().addCredits(this.creditsPerTime);
 			}
-		 				
 
-	 }
+			if (this.playerID == DefenderControl.PLAYER_TWO) {
+				gamelogic.getPlayerTwo().addCredits(this.creditsPerTime);
+			}
+
+			framesTemp = 0;
+		}
+
+	}
 
 }
