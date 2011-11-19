48979e7e78f9fca3e15576eca0e762cd13765b1f
kwoxer <kwoxer@googlemail.com>
2011-11-19 17:27:17 +0000

    TODO

---  defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/notmovable/Ground.java
+++  defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/notmovable/Ground.java
@@ -1,5 +1,7 @@
 package de.fhb.defenderTouch.units.notmovable;
 
+import java.util.Date;
+
 import de.fhb.defenderTouch.gamelogic.DefenderControl;
 import processing.core.PGraphics;
 
@@ -7,6 +9,11 @@ public class Ground extends Building {
 
 	public static final int PRICE = 50;
 	protected int size = 0;
+	
+	private int timeTillNextIncome = 5000;
+
+	private long startingTime = new Date().getTime();
+	private long tickerTime;
 
 	public Ground(int x, int y, int mode, int playerID, DefenderControl gamelogic) {
 		super(x, y, mode, playerID, gamelogic);
@@ -39,6 +46,31 @@ public class Ground extends Building {
 
 		graphics.resetMatrix();
 		graphics.stroke(0);
+
+	}
+	
+	public void calcNewPosition() {
+		tickerTime = new Date().getTime();
+
+		if (getNewGroundUnit(startingTime, tickerTime)) {
+			if (this.playerID == DefenderControl.PLAYER_ONE) {
+				// TODO
+			}
+
+			if (this.playerID == DefenderControl.PLAYER_TWO) {
+				//gamelogic.getPlayerTwo().addCredits(this.creditsPerTime);
+			}
+		}
+
+	}
+
+	public boolean getNewGroundUnit(long startingTime, long tickerTime) {
+		long helpTime = tickerTime - startingTime;
+		if (helpTime >= timeTillNextIncome) {
+			this.startingTime = new Date().getTime();
+			return true;
+		}
+		return false;
 
 	}
 
