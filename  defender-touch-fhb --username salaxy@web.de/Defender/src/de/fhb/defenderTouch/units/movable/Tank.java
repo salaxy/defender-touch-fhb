54f7546c8780c8940ae9291ed28b4ad7bab0a9e7
salaxy <salaxy@web.de>
2011-11-24 20:53:30 +0000

    Tank verbessert und Roundshoot implementiert

---  defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/movable/Tank.java
+++  defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/movable/Tank.java
@@ -4,6 +4,7 @@ import org.newdawn.slick.Graphics;
 
 import de.fhb.defenderTouch.gamelogic.DefenderControl;
 import de.fhb.defenderTouch.gamelogic.Player;
+import de.fhb.defenderTouch.units.amunition.RoundShoot;
 import de.fhb.defenderTouch.units.root.BaseUnit;
 
 public class Tank extends BaseUnit {
@@ -34,13 +35,32 @@ public class Tank extends BaseUnit {
 //			break;
 //		}
 
-		graphics.drawRect(0, size, size * 2, size * 3);
-		graphics.drawOval(0, size, size, size);
-		graphics.drawRect(0, -size/2, size/2, size * 2);
+//		graphics.drawRect(0, size, size * 2, size * 3);
+//		graphics.drawOval(0, size, size, size);
+//		graphics.drawRect(0, -size/2, size/2, size * 2);
+		
+		//ketten
+		graphics.drawRect(-12, -12,2, 25);
+		//chassi
+		graphics.drawRect(-10, -15,20, 30);
+		//Ketten
+		graphics.drawRect(10, -12,2, 25);
+		
+		//Turm
+		graphics.drawOval(-5, -10, 10, 10);
+		graphics.drawRect(-2, -8, 4, 30);
+
 
 		graphics.resetTransform();
 //		graphics.stroke(0);
 
 	}
+	
+	protected void startShoot(BaseUnit destinationUnit){
+		new RoundShoot((int) this.position.x, (int) this.position.y, BaseUnit.MODE_NORMAL, this.gamelogic.getPlayerSystem(), destinationUnit, this.damagePerHit,gamelogic);	
+
+	}
+	
+
 
 }
