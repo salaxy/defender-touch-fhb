f9b96c1dc0cb313a5add1c828dec8b1466df2840
salaxy@web.de <salaxy@web.de@b545fbdc-7392-c354-f912-c6a15991a2d7>
2011-12-01 18:27:06 +0000

    Code cleaning Fighter

--- trunk/ defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/movable/Fighter.java
+++ trunk/ defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/movable/Fighter.java
@@ -21,12 +21,7 @@ public class Fighter extends BaseUnit {
 
 	public void drawFigure(Graphics graphics) {
 
-		// graphics.stroke(0);
-		// graphics.strokeWeight(10);
-		// graphics.fill(0);
-		// this.entscheideLineFarbe(graphics);
 		graphics.scale(2, 2);
-		// graphics.triangle(-20,+20, 0, -20, +20, +20);
 		ArrayList<Vector2f> vektoren = new ArrayList<Vector2f>();
 		vektoren.add(new Vector2f(0, -8));
 		vektoren.add(new Vector2f(-8, 8));
@@ -40,24 +35,7 @@ public class Fighter extends BaseUnit {
 		GraphicTools.zeicheFigurNachVektoren(vektoren, graphics);
 
 		graphics.resetTransform();
-		// graphics.stroke(0);
 
 	}
-
-	// public void calcNewPosition(PApplet graphics){
-	// //wenn aktuelle position nicht nahe herankommt an ziel, dann weiter
-	// bewegen
-	// if(position.dist(destinationVector)>3){
-	// //Richtugnsvector berechnen
-	// direction=position.sub( destinationVector, position);
-	// //richtungsvector normieren
-	// direction.normalize();
-	// //normierten Richtungsvector zur position hinzurechnen
-	// position.add(direction.mult(direction, movementSpeed));
-	// //zeichne Schweif
-	// drawTail();
-	// }
-	// }
-	//
 
 }
