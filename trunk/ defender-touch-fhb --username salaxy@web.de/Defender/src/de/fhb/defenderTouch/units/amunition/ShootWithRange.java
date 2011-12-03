845b81c32f986874e6b0e16929d785e8795fe1e4
salaxy@web.de <salaxy@web.de@b545fbdc-7392-c354-f912-c6a15991a2d7>
2011-12-03 11:26:36 +0000

    Explosion des Shoots machbar gemacht

--- trunk/ defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/amunition/ShootWithRange.java
+++ trunk/ defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/amunition/ShootWithRange.java
@@ -1,6 +1,7 @@
 package de.fhb.defenderTouch.units.amunition;
 
 import org.newdawn.slick.Animation;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.geom.Vector2f;
 
 import de.fhb.defenderTouch.gamelogic.DefenderControl;
@@ -11,12 +12,14 @@ import de.fhb.defenderTouch.units.root.BaseUnit;
 import org.newdawn.slick.Graphics;
 public class ShootWithRange extends Shoot {
 
-	protected int maxRange = 300;
+	protected int maxRange = 400;
 	protected Vector2f startPostion;
-	private Animation smallExplosion;
-	private Player player;
-	private  Animations gl;
-	private Graphics graphics;
+	protected Animation smallExplosion;
+	protected Player player;
+	protected  Animations gl;
+	protected Graphics graphics;
+	protected boolean shouldBeDelete=false;
+	protected int nochXFramesZeichnen=10;
 
 	public ShootWithRange(int x, int y, int mode, Player player, BaseUnit destinationUnit, int damage, DefenderControl gamelogic) {
 
@@ -37,20 +40,58 @@ public class ShootWithRange extends Shoot {
 	}
 
 	public void delete() {
+
+		this.shouldBeDelete=true;
+//		super.delete();
+	}
+	
+	
+	public void drawFigure(Graphics graphics){
+
+		if(!shouldBeDelete){
+			super.drawFigure(graphics);
+		}
+		
+		if(shouldBeDelete&&nochXFramesZeichnen>0){
+			
+			//XXX hier die Explosion einbauen rotes Quasdrat ersetzen
+			
+//			GraphicTools.calcDrawTransformationForSlick(player, graphics, position);//nciht benutzen
+			graphics.setColor(Color.orange); //ersetzen
+			graphics.drawRect(0, 0, 10, 10);//ersetzen
+			
+			
+			graphics.resetTransform();
+			this.nochXFramesZeichnen--;
+		}
+		
+		if(shouldBeDelete&&nochXFramesZeichnen==0){
+			this.deleteWirklich();
+		}
+		
+		
 //		GraphicTools.calcDrawTransformationForSlick(player, graphics, position);
 //		smallExplosion.draw((-smallExplosion.getHeight() / 2) * player.getActualZoom(), (-smallExplosion.getWidth() / 2) * player.getActualZoom(), smallExplosion.getHeight()
 //				* player.getActualZoom(), smallExplosion.getWidth() * player.getActualZoom());
-////		graphics.resetTransform();
+//		graphics.resetTransform();
 //		System.out.println(1234);
 //		if (smallExplosion.getFrame() == gl.getNumberPictures() - 1) {
-////			smallExplosionPlaying = false;
+//			smallExplosionPlaying = false;
 //			
-////			smallExplosion.stop();
+//			smallExplosion.stop();
 //		}
-		// TODO hier Explosion einbauen
-		// *************************
 
-		// ************************* muss vor delete
+	}		
+	
+	public void deleteWirklich(){
 		super.delete();
+	}
+	
+	
+	protected void drawTail(Player player, Graphics graphics){
+		
+		if(!shouldBeDelete){
+			super.drawTail(player, graphics);
+		}
 	}
 }
