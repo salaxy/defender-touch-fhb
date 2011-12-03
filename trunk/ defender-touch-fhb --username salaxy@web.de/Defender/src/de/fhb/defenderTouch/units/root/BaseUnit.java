4f9bcd048d90af9670a6e9fa5c6c01f1b4a287e4
kwoxer <kwoxer@b545fbdc-7392-c354-f912-c6a15991a2d7>
2011-12-03 15:27:09 +0000

    Gegner kann nun nicht mehr gegnirische Einheiten Kontrollieren

--- trunk/ defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/root/BaseUnit.java
+++ trunk/ defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/root/BaseUnit.java
@@ -798,7 +798,7 @@ public class BaseUnit {
 	
 	public void playExplosionSound() {
 		try {
-			new SampleThread("/sounds/Explosion.mp3", 5.0f, true).start();
+			new SampleThread("/sounds/Explosion.mp3", 0.0f, true).start();
 		} catch (FormatProblemException e) {
 			e.printStackTrace();
 		}
