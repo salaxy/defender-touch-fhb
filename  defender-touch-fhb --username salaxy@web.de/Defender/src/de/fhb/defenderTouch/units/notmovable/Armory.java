375f983833aa5d771399f68bdbb1f5b3a8757a3c
kwoxer <kwoxer@b545fbdc-7392-c354-f912-c6a15991a2d7>
2011-12-08 00:41:25 +0000

    Unit fixes für morgen

---  defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/notmovable/Armory.java
+++  defender-touch-fhb --username salaxy@web.de/Defender/src/de/fhb/defenderTouch/units/notmovable/Armory.java
@@ -15,7 +15,7 @@ public class Armory extends Building {
 	public static final int PRICE = 70;
 	protected int size = 0;
 
-	private int timeTillNextSpawn = 1000;
+	private int timeTillNextSpawn = 10000;
 
 	private long startingTime = new Date().getTime();
 	private long tickerTime;
